9bd57186e1050d1caf4a490dd9557c2ac479814d
Eric Bailey <e.bailey@sportradar.com>
2018-08-24 18:41:22 -0500

    yq and parameter expansion ftw

--- outdated.sh
+++ outdated.sh
@@ -1,29 +1,15 @@
 #! /usr/bin/env nix-shell
-
-#! nix-shell --pure -i bash -p gawk kubernetes-helm
+#! nix-shell --pure -i bash -p gawk kubernetes-helm yq
 
 set -euo pipefail
 
 
-subcharts=$(helm dep list | tail -n+2 | head -n-1 | sort -u)
-
-
-current_version ()
-{
-    awk -v subchart="$1" 'subchart ~ $1 { print $2 }' <<<"$2"
-}
-
-
-find_latest ()
-{
-    helm search "$1" | awk -v subchart="$1" '$1 == subchart { print $2 }'
-}
-
-
-while IFS= read -r subchart
+while IFS= read -r dependency
 do
-    current=$(current_version "$subchart" "$subcharts")
-    latest=$(find_latest "$subchart")
+    subchart="${dependency%-*}"
+    current="${dependency##*-}"
+    latest=$(helm inspect chart "$subchart" | yq -r '.version')
+
     if [ "$current" == "$latest" ]
     then
         printf "%s is up to date.\\n" "$subchart"
@@ -31,4 +17,5 @@ do
         printf "Consider upgrading %s: %s -> %s.\\n" \
                "$subchart" "$current" "$latest"
     fi
-done < <(awk '{ sub("@","",$3); printf "%s/%s\n", $3, $1; } ' <<<"$subcharts")
+done < <(helm dep list | tail -n+2 | head -n-1 | sort -u | \
+             awk '{ sub("@","",$3); printf "%s/%s-%s\n", $3, $1, $2; }')

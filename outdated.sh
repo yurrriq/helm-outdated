9be6cfe54d3e599150960ae4a9daef682f535b19
Eric Bailey <e.bailey@sportradar.com>
2018-09-27 15:37:15 -0500

    shfmt -i 4 -w outdated.sh

--- outdated.sh
+++ outdated.sh
@@ -2,20 +2,18 @@
 
 set -euo pipefail
 
-while IFS= read -r dependency
-do
+while IFS= read -r dependency; do
     subchart="${dependency%-*}"
     current="${dependency##*-}"
     latest=$(helm inspect chart "$subchart" | yq -r '.version')
 
-    if [ "$current" == "$latest" ]
-    then
+    if [ "$current" == "$latest" ]; then
         printf "%s is up to date.\\n" "$subchart"
     else
         printf "Consider upgrading %s: %s -> %s.\\n" \
-               "$subchart" "$current" "$latest"
+            "$subchart" "$current" "$latest"
     fi
-done < <(helm dep list | \
-             grep -v WARNING | \
-             tail -n+2 | head -n-1 | sort -u | \
-             awk '{ sub("@","",$3); printf "%s/%s-%s\n", $3, $1, $2; }')
+done < <(helm dep list \
+    | grep -v WARNING \
+    | tail -n+2 | head -n-1 | sort -u \
+    | awk '{ sub("@","",$3); printf "%s/%s-%s\n", $3, $1, $2; }')
